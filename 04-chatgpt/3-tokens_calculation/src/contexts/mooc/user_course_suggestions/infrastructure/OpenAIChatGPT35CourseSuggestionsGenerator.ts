273a2a5a0c06b927195d4b378b18c451dbdd74da
Rafa Gómez <rgomezcasas@gmail.com>
2024-04-09 16:25:52 +0200

    feat(4-3): add example

--- 04-chatgpt/3-tokens_calculation/src/contexts/mooc/user_course_suggestions/infrastructure/OpenAIChatGPT35CourseSuggestionsGenerator.ts
+++ 04-chatgpt/3-tokens_calculation/src/contexts/mooc/user_course_suggestions/infrastructure/OpenAIChatGPT35CourseSuggestionsGenerator.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
-import { PromptTemplate, SystemMessagePromptTemplate } from "@langchain/core/prompts";
+import { Serialized } from "@langchain/core/load/serializable";
+import { LLMResult } from "@langchain/core/outputs";
+import { PromptTemplate } from "@langchain/core/prompts";
 import { RunnableSequence } from "@langchain/core/runnables";
 import { OpenAI } from "@langchain/openai";
 
@@ -27,29 +29,41 @@ export class OpenAIChatGPT35CourseSuggestionsGenerator implements CourseSuggesti
 
 	async generate(userCourseSuggestions: UserCourseSuggestions): Promise<string> {
 		const chain = RunnableSequence.from([
-			PromptTemplate.fromTemplate(`{completedCourses}`),
-			SystemMessagePromptTemplate.fromTemplate(
+			PromptTemplate.fromTemplate(
 				`* Actúas como un recomendador de cursos avanzado.
-                 * Solo debes sugerir cursos de la siguiente lista (IMPORTANTE: no incluyas cursos que no estén en la lista):
-                 ${this.existingCodelyCourses.map((course) => `\t- ${course}`).join("\n")}
-                 * Devuelve únicamente el listado de los 3 cursos recomendados, utilizando formato de lista en markdown.
-                 * Mantén la respuesta centrada en la recomendación, sin añadir agradecimientos o comentarios adicionales.
-                 * Asegúrate de que los cursos recomendados sean relevantes para el progreso del usuario, basándote en los cursos que ya ha completado.
-                 * Los cursos que ya ha completado el usuario son los que te proveerá.
-                 * Devuelve los cursos en castellano.
-                 * No puedes añadir cursos que el usuario ya ha completado.
-                 * No añadas ningún titular, devuelve sólo los nombres de los cursos.,
-                 * Devuelve sólo los nombres de los cursos, sin añadir información adicional.`,
+* Solo debes sugerir cursos de la siguiente lista (IMPORTANTE: no incluyas cursos que no estén en la lista):
+${this.existingCodelyCourses.map((course) => `\t- ${course}`).join("\n")}
+* Devuelve únicamente el listado de los 3 cursos recomendados, utilizando formato de lista en markdown.
+* Mantén la respuesta centrada en la recomendación, sin añadir agradecimientos o comentarios adicionales.
+* Asegúrate de que los cursos recomendados sean relevantes para el progreso del usuario, basándote en los cursos que ya ha completado.
+* Los cursos que ya ha completado el usuario son los que te proveerá.
+* Devuelve los cursos en castellano.
+* No puedes añadir cursos que el usuario ya ha completado.
+* No añadas ningún titular, devuelve sólo los nombres de los cursos.,
+* Devuelve sólo los nombres de los cursos, sin añadir información adicional.
+{completedCourses}`,
 			),
 			new OpenAI({
 				modelName: "gpt-3.5-turbo-0125",
 				openAIApiKey: process.env.OPENAI_API_KEY,
+				callbacks: [
+					{
+						handleLLMStart: (_llm: Serialized, prompts: string[]) => {
+							console.log("-- PROMPT --\n");
+							console.log(prompts[0]);
+						},
+						handleLLMEnd: (output: LLMResult) => {
+							console.log("\n\n-- RESULT --\n");
+							console.log(output.generations[0][0].text);
+						},
+					},
+				],
 			}),
 		]);
 
 		return await chain.invoke({
 			completedCourses: userCourseSuggestions.completedCourses
-				.map((course) => `* ${course}`)
+				.map((course) => `\t- ${course}`)
 				.join("\n"),
 		});
 	}
